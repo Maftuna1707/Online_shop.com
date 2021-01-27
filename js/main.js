09a9f976936b9b543b99426fd45d91674b2834a1
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-01-27 21:15:34 +0500

    Changed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function l_phones() {
+    document.getElementById("menu_right1").className = "bi bi-chevron-down"
     document.getElementById("companies_p").style.display = "block";
 }
 function l_computers() {
+    document.getElementById("menu_right2").className = "bi bi-chevron-down"
     document.getElementById("companies_c").style.display = "block";
 }
