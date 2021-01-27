1d27f955b11ab201992a0ec1ebbec1af10f8fe87
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-01-27 20:29:29 +0500

    Changed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-function press(){
-    document.getElementsByClassName("dropdown-content").style.display = "block"
+function l_phones() {
+    document.getElementById("companies_p").style.display = "block";
+}
+function l_computers() {
+    document.getElementById("companies_c").style.display = "block";
 }
