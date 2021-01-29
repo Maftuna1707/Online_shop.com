dd9489cac99f9fd68533dc64d89a31b98de94046
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-01-29 22:18:53 +0500

    Changed

--- js/main.js
+++ js/main.js
@@ -20,9 +20,15 @@ function iPhone() {
 }
 
 let iphone8 = {
+    memory: '64GB',
+    name: 'iPhone 8',
+    price: '$299.99',
     src: 'images/apple8.png'
 }
 let iphone11pro = {
+    memory: '64GB',
+    name: 'iPhone 8',
+    price: '$299.99',
     src: 'images/apple11pro.png'
 }
 let iphoneXS = {
